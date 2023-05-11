c212ff55f4fc701d9f6b03e9b678fe0705688b05
Victor Boudet <victor.boudet@epitech.eu>
2023-05-11 18:52:02 +0200

    [ REMAKE ] remake from scratch exec_command function and all dependancies

--- pipe.c
+++ pipe.c
@@ -17,57 +17,19 @@
 void double_close(int fd1, int fd2);
 format_t *format(char **str);
 char **get_arg(char *str, int a);
+int get_ind(char *str);
 
-void action(int nb, char **commands[], int i)
+static int set_var(char *str, bool tr)
 {
-    int out_fd;
-    switch (nb) {
-        case 2:
-            out_fd = open(commands[i + 1][0], O_WRONLY | O_CREAT | O_APPEND,
-                0644);
-            dup2(out_fd, STDOUT_FILENO);
-            close(out_fd);
-            break;
-        case 4:
-            out_fd = open(commands[i + 1][0], O_WRONLY | O_CREAT | O_TRUNC,
-                0644);
-            dup2(out_fd, STDOUT_FILENO);
-            close(out_fd);
-            break;
+    static int act = 0;
+    if (tr == true) {
+        act = get_ind(str);
+        return 0;
     }
+    return act;
 }
 
-void generic_pipe(char **commands[], int num_commands, int *tab)
-{
-    int fd[2], in_fd = dup(STDIN_FILENO);
-    pid_t pid;
-    for (int i = 0; i < num_commands - 1; i++) {
-        pipe(fd);
-        pid = fork();
-        if (pid == 0) {
-            dup2(in_fd, STDIN_FILENO);
-            close(in_fd);
-            dup2(fd[1], STDOUT_FILENO);
-            double_close(fd[0], fd[1]);
-            action(tab[i], commands, i);
-            exit(last_exec(commands[i]));
-        }
-        double_close(in_fd, fd[1]);
-        in_fd = fd[0];
-    }
-    dup2(in_fd, STDIN_FILENO);
-    close(in_fd);
-    exit(last_exec(commands[num_commands - 1]));
-}
-
-int double_array_len(char ***array)
-{
-    int i = 0;
-    for (i = 0; array[i] != NULL; i++);
-    return i;
-}
-
-char **strtoktok(char **array)
+static char **strtoktok(char **array)
 {
     static char **save = NULL;
     if (array != NULL && save == NULL) {
@@ -77,27 +39,52 @@ char **strtoktok(char **array)
     if (save == NULL)
         return NULL;
     int i = 0;
-    for (i = 0; save[i] != NULL && save[i][0] != -64; i++);
-    char **ret = calloc(i, sizeof(char **));
-    for (i = 0; save != NULL && save[0][0] != -64; i++) {
-        ret[i] = *save;
-        save++;
+    for (i = 0; save[i] != NULL && save[i][0] != MAIN_SEP; i++);
+    char **ret = calloc(i + 1, sizeof(char *));
+    if (ret == NULL) {
+        return NULL;
     }
-    return save;
+    for (i = 0; save[i] != NULL && save[i][0] != MAIN_SEP; i++)
+        ret[i] = save[i];
+    if (save[i] != NULL) set_var(save[i], true);
+    ret[i] = NULL;
+    save += i + 1;
+    if (save[-1] == NULL) save = NULL;
+    return ret;
+}
+
+static bool act(int ret)
+{
+    int a = set_var(NULL, false);
+    if (a == 0) {
+        return true;
+    }
+    if (a == 6 && ret == 0) {
+        return true;
+    }
+    if (a == 7 && ret != 0) {
+        return true;
+    }
+    return false;
 }
 
 static int exec_command(char **arg, int ret)
 {
     int reto = 0;
-    if (arg[0] == NULL) {
+    if (arg[0] == NULL)
         return 0;
-    }
     strtoktok(arg);
     char **tmp = NULL;
-    while ((tmp = strtoktok(NULL)) != NULL) {
+    for (int i = 0;(tmp = strtoktok(NULL)) != NULL; i++) {
+        if (act(ret) && i) break;
+        form_t *f = transform(tmp);
+        if (f->cmd[1] == NULL) {
+            ret = last_exec(tmp);
+            continue;
+        }
         pid_t pid = fork();
         if (pid == 0) {
-            generic_pipe(tmp, double_array_len(tmp), f->tab);
+            generic_pipe(f->cmd, f->ns + 1, f->sep);
         } else {
             wait(&reto);
         }
@@ -108,10 +95,6 @@ static int exec_command(char **arg, int ret)
 int check_line(char *line, int ret)
 {
     char **arg = clean_arg(line);
-    line = strtok(line, ";");
-    while (line != NULL && line[0] != 10) {
-        ret = exec_command(arg, ret);
-        line = strtok(NULL, ";");
-    }
+    ret = exec_command(arg, ret);
     return ret;
 }
