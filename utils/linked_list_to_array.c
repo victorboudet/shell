b36770fa62fdb379d653264485deff043a01d522
Victor Boudet <victor.boudet@epitech.eu>
2023-05-11 18:57:04 +0200

    [ CREATE ] creation of a function to convert linked list to char 42sh clean_arg.c functions handlers include init_env.c main.c Makefile pipe.c pipeu.c star test.c transform.c utils

---
+++ utils/linked_list_to_array.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** B-PSU-200-MPL-2-1-42sh-kylian.labrador
+** File description:
+** linked_list_to_array
+*/
+
+#include "shell.h"
+
+char **linked_list_to_array(arg_t *first)
+{
+    int len = 0;
+    arg_t *tmp = first;
+    for (; tmp != NULL; tmp = tmp->next) {
+        len++;
+    }
+    char **ret = calloc(len + 1, sizeof(char *));
+    tmp = first;
+    for (int i = 0; tmp != NULL; tmp = tmp->next, i++) {
+        ret[i] = tmp->curr;
+    }
+    return ret;
+}
